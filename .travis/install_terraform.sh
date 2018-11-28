8a763fb89ea90e4572fce06f7faa2c6cf5a49bb1
Clinton Blackburn <clinton.blackburn@gmail.com>
2018-11-27 22:21:09 -0800

    Updated Travis Terraform install script
    The script now overwrites when unzipping to avoid waiting for input.

--- .travis/install_terraform.sh
+++ .travis/install_terraform.sh
@@ -3,7 +3,6 @@
 set -e
 
 wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
-unzip terraform_0.11.10_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
+sudo unzip -o terraform_0.11.10_linux_amd64.zip -d /usr/local/bin/
 rm terraform_0.11.10_linux_amd64.zip
 terraform --version
