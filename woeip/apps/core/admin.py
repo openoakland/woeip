9dd6f89784fd9f8b397ff425640ec559fd39bf4f
Robert Gibboni <galileo@gmail.com>
2018-12-16 13:46:41 -0800

    Database schema added to models.py + admin interface

--- woeip/apps/core/admin.py
+++ woeip/apps/core/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from woeip.apps.core.models import User
+from woeip.apps.core.models import (
+    Data, Device, Organization, Participant, RoleEnum, RouteEnum, Sensor, Session, SessionData, User
+)
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Organization)
+admin.site.register(RoleEnum)
+admin.site.register(Participant)
+admin.site.register(RouteEnum)
+admin.site.register(Session)
+admin.site.register(Device)
+admin.site.register(Sensor)
+admin.site.register(SessionData)
+admin.site.register(Data)
