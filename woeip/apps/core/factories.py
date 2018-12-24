d86ffd0714546d39acee14595be651ef4de93d3a
Robert Gibboni <galileo@gmail.com>
2018-12-23 20:24:25 -0800

    Fixed User model and factory

--- woeip/apps/core/factories.py
+++ woeip/apps/core/factories.py
@@ -1,11 +1,19 @@
 import factory
 
-from woeip.apps.core.models import User
+from ..models import User
+
+USER_PASSWORD = 'password'
 
 
 class UserFactory(factory.DjangoModelFactory):
+    username = factory.Sequence(lambda n: 'user_%d' % n)
+    password = factory.PostGenerationMethodCall('set_password', USER_PASSWORD)
+    is_active = True
+    is_superuser = False
+    is_staff = False
+    email = factory.Faker('email')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+
     class Meta:
         model = User
-
-    name = factory.Faker('name')
-    email = factory.Faker('email')
