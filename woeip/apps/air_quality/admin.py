ca4d9f7dbf6851b30a02f4bb804b9a51caf5eca0
Robert Gibboni <galileo@gmail.com>
2019-01-04 09:11:41 -0800

    Register session data with decorator

--- woeip/apps/air_quality/admin.py
+++ woeip/apps/air_quality/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from woeip.apps.air_quality.models import Data, Device, Route, Sensor, Session, SessionData
 
 
+@admin.register(SessionData)
 class SessionDataAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'modified', )
 
@@ -11,5 +12,4 @@ admin.site.register(Route)
 admin.site.register(Session)
 admin.site.register(Device)
 admin.site.register(Sensor)
-admin.site.register(SessionData, SessionDataAdmin)
 admin.site.register(Data)
