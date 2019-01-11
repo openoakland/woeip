d91e20bf186a73d14fbb07dedcda94a061b6706c
Natalia Bilenko <nbilenko@gmail.com>
2019-01-11 15:22:38 -0800

    Update how storage settings are handled by default

--- woeip/settings.py
+++ woeip/settings.py
@@ -1,4 +1,5 @@
 import environ
+from django.core.exceptions import ImproperlyConfigured
 
 repo_root = environ.Path(__file__) - 2
 project_root = environ.Path(__file__) - 1
@@ -41,14 +42,20 @@ LOCAL_APPS = [
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
+DEFAULT_FILE_STORAGE = env(
+    'DEFAULT_FILE_STORAGE', default='storages.backends.s3boto.S3BotoStorage')
+AWS_S3_ACCESS_KEY_ID = env('AWS_S3_ACCESS_KEY_ID', default=None)
+AWS_S3_SECRET_ACCESS_KEY = env('AWS_S3_SECRET_ACCESS_KEY', default=None)
+AWS_S3_HOST = 's3.us-east-2.amazonaws.com'
+AWS_STORAGE_BUCKET_NAME = 'woaq'
+AWS_DEFAULT_ACL = None
 
-if len(env('AWS_S3_ACCESS_KEY_ID')) > 10:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    AWS_S3_HOST = 's3.us-east-2.amazonaws.com'
-    AWS_S3_ACCESS_KEY_ID = env('AWS_S3_ACCESS_KEY_ID')
-    AWS_S3_SECRET_ACCESS_KEY = env('AWS_S3_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = 'woaq'
-    AWS_DEFAULT_ACL = None
+if (DEFAULT_FILE_STORAGE == 'storages.backends.s3boto.S3BotoStorage' and
+        (AWS_S3_ACCESS_KEY_ID is None or AWS_S3_SECRET_ACCESS_KEY is None)):
+    raise ImproperlyConfigured(
+        'Environmental variables AWS_S3_ACCESS_KEY_ID and AWS_S3_SECRET_ACCESS_KEY '
+        'must be configured to use S3 storage')
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
