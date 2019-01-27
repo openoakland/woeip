abeda1da2c51b87f2f551deb57440d3968978c30
Robert Gibboni <galileo@gmail.com>
2019-01-27 22:02:52 +0000

    Use pytest fixtures to avoid code duplication

--- woeip/apps/air_quality/tests/test_dustrak.py
+++ woeip/apps/air_quality/tests/test_dustrak.py
@@ -1,3 +1,4 @@
+# pylint: disable=W0621
 from pathlib import Path
 
 import numpy as np
@@ -7,43 +8,44 @@ import pytest
 from woeip.apps.air_quality import dustrak, models
 from woeip.apps.air_quality.tests import factories
 
+test_data_directory = Path(__file__).parent / 'data'
 
-def test_joiner():
+
+@pytest.fixture
+def target_data():
+    """Load the target output"""
+    target_data = pd.read_csv(test_data_directory / 'joined.csv', parse_dates=['time'])
+    target_data.set_index('time', inplace=True)
+    target_data = target_data.tz_localize('UTC')
+    target_data.reset_index(inplace=True)
+
+    return target_data
+
+
+def test_joiner(target_data):
     """Test the output of the function that joins GPS and air quality measurements based on time stamps"""
-    test_data_directory = Path(__file__).parent / 'data'
     dustrak_file_handle = open(test_data_directory / 'dustrak.csv', 'r')
     gps_file_handle = open(test_data_directory / 'gps.log', 'r')
 
     with pytest.warns(UserWarning):
-        test_joined_data = dustrak.join(dustrak_file_handle, gps_file_handle)
+        joined_data = dustrak.join(dustrak_file_handle, gps_file_handle)
 
     dustrak_file_handle.close()
     gps_file_handle.close()
 
-    joined_data = pd.read_csv(test_data_directory / 'joined.csv', parse_dates=['time'])
-    joined_data.set_index('time', inplace=True)
-    joined_data = joined_data.tz_localize('UTC')
-    joined_data.reset_index(inplace=True)
-
-    for column in joined_data:
+    for column in target_data:
         if column == 'time':
-            assert all(joined_data[column] == test_joined_data[column])
+            assert all(target_data[column] == joined_data[column])
         else:
-            assert np.allclose(joined_data[column], test_joined_data[column])
+            assert np.allclose(target_data[column], joined_data[column])
 
 
 @pytest.mark.django_db
-def test_save():
+def test_save(target_data):
     """Test ability to save a session of joined GPS/air quality measurement data to the database"""
-    test_data_directory = Path(__file__).parent / 'data'
     session_data = factories.SessionDataFactory()
     session_data.save()
 
-    joined_data = pd.read_csv(test_data_directory / 'joined.csv', parse_dates=['time'])
-    joined_data.set_index('time', inplace=True)
-    joined_data = joined_data.tz_localize('UTC')
-    joined_data.reset_index(inplace=True)
+    dustrak.save(target_data, session_data)
 
-    dustrak.save(joined_data, session_data)
-
-    assert len(models.Data.objects.all()) == len(joined_data)
+    assert len(models.Data.objects.all()) == len(target_data)
