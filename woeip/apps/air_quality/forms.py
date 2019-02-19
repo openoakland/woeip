d2a611994439bb2c5087601d3f0048a9d35ae3a3
tangoyankee <miller.tim108@gmail.com>
2019-02-19 08:53:26 -0800

    light edits to comments and implement @login_required

--- woeip/apps/air_quality/forms.py
+++ woeip/apps/air_quality/forms.py
@@ -1,4 +1,4 @@
-## TODO: Linting marked as unused... BaseInlineFormSet, inlineFormSet
+# TODO: Linting marked as unused... BaseInlineFormSet, inlineFormSet
 from django import forms
 from django.forms.models import BaseInlineFormSet, inlineformset_factory
 
@@ -8,8 +8,8 @@ from woeip.apps.air_quality import models
 class SessionForm(forms.ModelForm):
     class Meta:
         model = models.Session
-        ## TODO: On the client side, create an inteface to guide the user to the
-        ## correct date format
+        # TODO: On the client side, create an inteface to guide the user to the
+        # correct date format
         fields = ('collected_by', 'date_collected', 'route')
 
 
@@ -18,6 +18,6 @@ class DustrakSessionForm(SessionForm):
     gps = forms.FileField()
 
     class Meta(SessionForm.Meta):
-        ## TODO: Advise on the client side that the devices are automatically set
-        ## to GPS and Dustrak.
+        # TODO: Advise on the client side that the devices are automatically set
+        # to GPS and Dustrak.
         fields = SessionForm.Meta.fields + ('air_quality', 'gps')
--- woeip/apps/air_quality/views.py
+++ woeip/apps/air_quality/views.py
@@ -1,9 +1,10 @@
-## TODO: imports pylint lists as unused: datetime, authenticate, login, User
+# TODO: imports pylint lists as unused: datetime, authenticate, login, User
 import datetime
 import logging
 
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
 from woeip.apps.air_quality import dustrak, forms, models
@@ -15,61 +16,55 @@ logger = logging.getLogger(__name__)
 def index(request):
     return render(request, 'index.html')
 
-## TODO: Refractor to decorater '@login-required'
-## The warning message will no longer appear. However, it will default to the Django security 
-## control. It will also make the view more concise
+
+@login_required
 def upload(request):
     """Upload data for a session collected using the Dustrak air quality device and a separate GPS
     log file. Creates a new Session instance, two SessionData instances, and Data instances for
     each sample.
     """
-    ## TODO: Linting message; three no-else-returns
-    if request.user.is_authenticated:
-        if request.method == 'POST':
-            form = forms.DustrakSessionForm(request.POST, request.FILES)
-            if form.is_valid():
-                ## TODO: Outsource this air quality and gps object creation to factory file 
-                ## (Unsure about this)
-                form.save()
-                ## TODO: Script,from a csv, standardized data for dustrak, gps, and any other
-                ## objects where the values are already known (such as routes)
-                air_sensor = models.Sensor.objects.get(name='Dustrak')
-                gps_sensor = models.Sensor.objects.get(name='GPS')
+    # TODO: Linting message; two no-else-returns
+    if request.method == 'POST':
+        form = forms.DustrakSessionForm(request.POST, request.FILES)
+        if form.is_valid():
+            # TODO: Outsource this air quality and gps object creation to factory file
+            # (Unsure about this)
+            form.save()
+            # TODO: Script,from a csv, standardized data for dustrak, gps, and any other
+            # objects where the values are already known (such as routes)
+            air_sensor = models.Sensor.objects.get(name='Dustrak')
+            gps_sensor = models.Sensor.objects.get(name='GPS')
 
-                air_quality = models.SessionData(upload=request.FILES['air_quality'],
-                                                 sensor=air_sensor,
-                                                 session=form.instance,
-                                                 uploaded_by=request.user)
-                gps = models.SessionData(upload=request.FILES['gps'],
-                                         sensor=gps_sensor,
-                                         session=form.instance,
-                                         uploaded_by=request.user)
+            air_quality = models.SessionData(upload=request.FILES['air_quality'],
+                                             sensor=air_sensor,
+                                             session=form.instance,
+                                             uploaded_by=request.user)
+            gps = models.SessionData(upload=request.FILES['gps'],
+                                     sensor=gps_sensor,
+                                     session=form.instance,
+                                     uploaded_by=request.user)
 
-                try:
-                    joined_data = dustrak.join(request.FILES['air_quality'].file,
-                                               request.FILES['gps'].file)
-                ## TODO: Research possible exceptions. Send specific error messages
-                except Exception as e:
-                    messages.add_message(request, messages.ERROR, f'File upload failed, error: {e}')
-                    return redirect('upload')
-
-                air_quality.save()
-                gps.save()
-                dustrak.save(joined_data, air_quality)
-
-                messages.add_message(request, messages.SUCCESS, 'Files successfully uploaded')
+            try:
+                joined_data = dustrak.join(request.FILES['air_quality'].file,
+                                           request.FILES['gps'].file)
+            # TODO: Research possible exceptions. Send specific error messages
+            except Exception as e:
+                messages.add_message(request, messages.ERROR, f'File upload failed, error: {e}')
                 return redirect('upload')
 
-            else:
-                messages.add_message(request, messages.ERROR, f'File upload failed, error: {form.errors}')
-                return redirect('upload')
+            air_quality.save()
+            gps.save()
+            dustrak.save(joined_data, air_quality)
+
+            messages.add_message(request, messages.SUCCESS, 'Files successfully uploaded')
+            return redirect('upload')
 
         else:
-            form = forms.DustrakSessionForm(initial={'collected_by': request.user})
-
-            return render(request, 'upload.html',
-                          {'user': request.user, 'form': form})
+            messages.add_message(request, messages.ERROR, f'File upload failed, error: {form.errors}')
+            return redirect('upload')
 
     else:
-        messages.add_message(request, messages.WARNING, 'Please log in to continue')
-        return redirect(f'/accounts/login?next={request.path}')
+        form = forms.DustrakSessionForm(initial={'collected_by': request.user})
+
+    return render(request, 'upload.html',
+                  {'user': request.user, 'form': form})
