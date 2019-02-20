210623dd31cd2a01502aac427d1181169d037894
tangoyankee <miller.tim108@gmail.com>
2019-02-20 08:45:10 -0800

    remove todos and place them in github issues

--- woeip/apps/air_quality/forms.py
+++ woeip/apps/air_quality/forms.py
@@ -3,14 +3,9 @@ from django import forms
 from woeip.apps.air_quality import models
 
 
-# TODO: Include Sensor fields that are seen by client when uploading the files.
-# 1) It is more transparent to the user what data is being loaded and why.
-# 2) It is easier to generalize the website, once other sensors are available.
 class SessionForm(forms.ModelForm):
     class Meta:
         model = models.Session
-        # TODO: On the client side, create an inteface to guide the user to the
-        # correct date format
         fields = ('collected_by', 'date_collected', 'route')
 
 
