dbc6e8ead7b887f5211566a8a24d112ca4acfd78
tangoyankee <miller.tim108@gmail.com>
2019-02-21 23:19:05 -0800

    exit code earlier. narrow which errors are caught

--- woeip/apps/air_quality/views.py
+++ woeip/apps/air_quality/views.py
@@ -3,7 +3,6 @@ import logging
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
-from django.db import Error
 from django.shortcuts import redirect, render
 from django.utils.encoding import force_text
 
@@ -31,26 +30,26 @@ def upload(request):
                 air_sensor = models.Sensor.objects.get(name='Dustrak')
                 gps_sensor = models.Sensor.objects.get(name='GPS')
 
-                air_quality = models.SessionData(upload=request.FILES['air_quality'],
-                                                 sensor=air_sensor,
-                                                 session=form_instance,
-                                                 uploaded_by=request_user)
-                gps = models.SessionData(upload=request.FILES['gps'],
-                                         sensor=gps_sensor,
-                                         session=form_instance,
-                                         uploaded_by=request_user)
-
-                air_quality_contents = force_text(request.FILES['air_quality'].read())
-                _, air_quality_data = dustrak.load_dustrak(air_quality_contents, form.data['timezone'])
-
-                gps_contents = force_text(request.FILES['gps'].read())
-                gps_data = dustrak.load_gps(gps_contents)
-                joined_data = dustrak.join(air_quality_data, gps_data)
-
-            except (UnboundLocalError, ObjectDoesNotExist, Error) as e:
+            except (ObjectDoesNotExist) as e:
                 messages.add_message(request, messages.ERROR, f'File upload failed, error: {e}')
-                logger.exception(e)
+                logger.exception('Could not find sensor information')
                 return redirect('upload')
+
+            air_quality = models.SessionData(upload=request.FILES['air_quality'],
+                                             sensor=air_sensor,
+                                             session=form_instance,
+                                             uploaded_by=request_user)
+            gps = models.SessionData(upload=request.FILES['gps'],
+                                     sensor=gps_sensor,
+                                     session=form_instance,
+                                     uploaded_by=request_user)
+
+            air_quality_contents = force_text(request.FILES['air_quality'].read())
+            _, air_quality_data = dustrak.load_dustrak(air_quality_contents, form.data['timezone'])
+
+            gps_contents = force_text(request.FILES['gps'].read())
+            gps_data = dustrak.load_gps(gps_contents)
+            joined_data = dustrak.join(air_quality_data, gps_data)
 
             air_quality.save()
             gps.save()
