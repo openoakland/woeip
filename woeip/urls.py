cd93e1691b034d13bcd07000d4fec8e37c3df17c
tangoyankee <miller.tim108@gmail.com>
2019-04-17 20:27:30 -0700

    give upload page the root url. delete template and url for index page

--- woeip/urls.py
+++ woeip/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from .apps.air_quality.views import index, upload
+from .apps.air_quality.views import upload
 from .apps.core.views import health
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', upload, name='upload'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-    path('files/upload/', upload, name='upload'),
     path('health/', health, name='health'),
 ]
