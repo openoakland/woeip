236ac2153c0f6b096f5f418588d77e889e0c506f
tangoyankee <miller.tim108@gmail.com>
2019-07-03 21:16:12 -0700

    javascript responsive footer

--- woeip/static/js/app.js
+++ woeip/static/js/app.js
@@ -6,3 +6,20 @@ function showPassword() {
         userPassInput.type = "password";
     }
 }
+
+function footerAlign(){
+    $('footer').css('height', 'auto');
+    var footerHeight = $('footer').outerHeight();
+    $('body').css('padding-bottom', footerHeight);
+    $('footer').css('height', footerHeight);
+}
+
+$(document).ready(()=>{
+    footerAlign();
+})
+
+$(window).resize(()=>{$(document).ready(()=>{
+    footerAlign();
+})
+    footerAlign();
+})
