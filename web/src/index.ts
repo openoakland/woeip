6d522ce48261f69f45a551320b17f277d66619ab
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2019-07-16 19:16:13 -0700

    Fix typescript signature match error

--- web/src/index.ts
+++ web/src/index.ts
@@ -20,10 +20,4 @@ const port = process.env.PORT || 3000;
 
 export default express()
   .use((req, res) => app.handle(req, res))
-  .listen(port, (err: Error) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(`> Started on port ${port}`);
-  });
+  .listen(port, () =>  console.log(`> Started on port ${port}`));
