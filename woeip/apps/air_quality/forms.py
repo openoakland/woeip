7e8965c035d4eb6243b57ea8c848e4b6767ca772
Natalia Bilenko <nbilenko@gmail.com>
2019-08-06 21:02:05 -0700

    re-adding placeholder views

--- woeip/apps/air_quality/forms.py
+++ woeip/apps/air_quality/forms.py
@@ -3,3 +3,8 @@ from django import forms
 from woeip.apps.air_quality import models
 
 
+class CollectionForm(forms.ModelForm):
+    """Both an air quality and a gps file are required for the new collection."""
+    class Meta():
+        model = models.Collection
+        fields = ('starts_at', 'ends_at')
--- woeip/apps/air_quality/views.py
+++ woeip/apps/air_quality/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
 from django.views import View
 
-# from .forms import SessionDataForm
-# from .models import SessionData
+from .forms import CollectionForm
+from .models import Collection
 
 logger = logging.getLogger(__name__)
 
@@ -14,5 +14,28 @@ logger = logging.getLogger(__name__)
 class Upload(LoginRequiredMixin, View):
     def get(self, request):
         """Present file upload form to user"""
-        return 'ok'
+        return render(self.request, 'air_quality/upload.html', {
+            'form': CollectionForm
+        })
 
+    def post(self, request):
+        """Save files to SessionData table"""
+        files = self.request.FILES
+        form = CollectionForm(self.request.POST, files)
+        if form.is_valid():
+            form.save()
+            # return redirect('view')
+        else:
+            messages.add_message(self.request, messages.ERROR, 'File upload error')
+            return render(self.request, 'air_quality/upload.html', {
+                'form': CollectionForm
+            })
+
+
+class ViewCollection(View):
+    """Provide temporary development page to view all uploaded SessionDatas."""
+    def get(self, request):
+        collection_list = Collection.objects.all()
+        return render(self.request, 'air_quality/view.html', {
+            'collection_list': collection_list
+        })
