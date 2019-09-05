b9f2bebf67d31e1a2f04fdc3e9e2707c012fb44e
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2019-09-04 23:55:59 -0700

    Fix path resolution

--- web/razzle.config.js
+++ web/razzle.config.js
@@ -1,4 +1,6 @@
-'use strict';
+'use strict'
+
+const path = require('path')
 
 module.exports = {
   plugins: ['typescript'],
@@ -7,8 +9,16 @@ module.exports = {
 
     if (config.devServer) {
       // Handle HMR within docker env: https://github.com/jaredpalmer/razzle/issues/416
-      config.devServer.watchOptions['poll'] = 1000;
-      config.devServer.watchOptions['aggregateTimeout'] = 300;
+      config.devServer.watchOptions['poll'] = 1000
+      config.devServer.watchOptions['aggregateTimeout'] = 300
+    }
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      components: path.resolve('./src/components'),
+      containers: path.resolve('./src/containers'),
+      pages: path.resolve('./src/pages'),
+      routes: path.resolve('./src/routes')
     }
 
     return config
