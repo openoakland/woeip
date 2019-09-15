e28636d2e6ad8fea4ec3899bbc00dbaee0cf751d
Robert Gibboni <galileo@gmail.com>
2019-09-15 16:17:29 -0700

    Add static to urls for serving static during development

--- api/woeip/urls.py
+++ api/woeip/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
@@ -22,4 +24,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
