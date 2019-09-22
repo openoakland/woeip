c5df3494092bed61101d8f454cb52807eb83ac61
Robert Gibboni <galileo@gmail.com>
2019-09-21 23:45:12 -0700

    Revert adding static to urls for development

--- api/woeip/urls.py
+++ api/woeip/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
@@ -23,4 +21,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
