7e40bc9e726f3335cf3d1edcb699823bfaaf4cf2
Robert Gibboni <galileo@gmail.com>
2019-09-21 23:32:16 -0700

    Break up api tests into separate functions

--- api/woeip/apps/air_quality/tests/test_api.py
+++ api/woeip/apps/air_quality/tests/test_api.py
@@ -1,5 +1,4 @@
-import pytest
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIRequestFactory, APITestCase
 
 from woeip.apps.air_quality import views
 from woeip.apps.air_quality.tests import factories
@@ -7,35 +6,35 @@ from woeip.apps.air_quality.tests import factories
 request_factory = APIRequestFactory()
 
 
-@pytest.mark.django_db
-def test_collection():
-    collection_file = factories.CollectionFileFactory()
-    pollutant = factories.PollutantFactory()
+class TestCollection(APITestCase):
+    def setUp(self):
+        self.collection_file = factories.CollectionFileFactory()
+        self.pollutant = factories.PollutantFactory()
+        self.n_values = 10
+        self.test_values = []
+        for _ in range(self.n_values):
+            pollutant_value = factories.PollutantValueFactory(
+                collection_file=self.collection_file,
+                pollutant=self.pollutant,
+            )
+            self.test_values.append(pollutant_value.value)
 
-    n_values = 10
-    test_values = []
-    for _ in range(n_values):
-        pollutant_value = factories.PollutantValueFactory(
-            collection_file=collection_file,
-            pollutant=pollutant,
-        )
-        test_values.append(pollutant_value.value)
+    def test_get_collection_list(self):
+        request = request_factory.get("/collection")
+        view = views.CollectionViewSet.as_view({"get": "list"})
+        response = view(request)
+        assert response.status_code == 200
 
-    request = request_factory.get("/collection")
-    view = views.CollectionViewSet.as_view({"get": "list"})
-    response = view(request)
-    assert response.status_code == 200
+    def test_get_collection_retrieve(self):
+        request = request_factory.get("/collection")
+        view = views.CollectionViewSet.as_view(actions={"get": "retrieve"})
+        response = view(request, pk=self.collection_file.collection.pk)
+        assert response.status_code == 200
 
-    request = request_factory.get("/collection")
-    view = views.CollectionViewSet.as_view(actions={"get": "retrieve"})
-    response = view(request, pk=collection_file.collection.pk)
-    assert response.status_code == 200
-
-    request = request_factory.get("/collection")
-    view = views.CollectionViewSet.as_view(actions={"get": "data"})
-    response = view(request, pk=collection_file.collection.pk)
-    assert response.status_code == 200
-
-    stored_values = response.data["pollutant_values"]
-    for (test_value, stored_value) in zip(test_values, stored_values):
-        assert test_value == stored_value["value"]
+    def test_get_collection_data(self):
+        request = request_factory.get("/collection")
+        view = views.CollectionViewSet.as_view(actions={"get": "data"})
+        response = view(request, pk=self.collection_file.collection.pk)
+        assert response.status_code == 200
+        stored_values = [pv["value"] for pv in response.data["pollutant_values"]]
+        assert stored_values == self.test_values
