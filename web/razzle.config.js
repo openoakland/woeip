985a6c1f1dab54dc285b6cff204876937ead7eb9
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2019-11-22 14:56:00 -0800

    Add styling libraries

--- web/razzle.config.js
+++ web/razzle.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  plugins: ['typescript'],
+  plugins: ['scss', 'typescript'],
   modify: (defaultConfig, { target, dev }) => {
     const config = defaultConfig
 
