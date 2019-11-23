7c8eef876370310b17d1847625b57ac3d80b395b
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2019-11-22 16:32:30 -0800

    Create routing config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'emotion-theming'
-import Home from './Home'
 import 'semantic-ui-css/semantic.min.css'
 import './App.scss'
 import theme from 'theme'
+import routes from 'routes'
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <Switch>
-      <Route exact path='/' component={Home} />
+      {routes.map(route => (
+        <Route key={route.path} {...route} />
+      ))}
     </Switch>
   </ThemeProvider>
 )
---
+++ web/src/routes/index.ts
@@ -0,0 +1,32 @@
+import Home from '../Home'
+
+interface RouteConfig {
+  path?: string
+  exact?: boolean
+  component: React.ComponentClass<any> | React.FunctionComponent<any>
+}
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    exact: true,
+    component: Home
+  },
+  {
+    path: '/about',
+    exact: true,
+    component: Home
+  },
+  {
+    path: '/upload',
+    exact: true,
+    component: Home
+  },
+  {
+    path: '/maps',
+    exact: true,
+    component: Home
+  }
+]
+
+export default routes
