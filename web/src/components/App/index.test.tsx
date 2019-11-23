48182a355a1ef05058e3cc78fa0f5b3d7f39a6d5
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2019-11-22 16:55:35 -0800

    Move content into components directory

--- web/src/components/App/index.test.tsx
+++ web/src/components/App/index.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import App from './App'
+import App from 'components/App'
 
 import { MemoryRouter } from 'react-router-dom'
 
--- web/src/components/App/index.tsx
+++ web/src/components/App/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'emotion-theming'
 import 'semantic-ui-css/semantic.min.css'
-import './App.scss'
+import './index.scss'
 import theme from 'theme'
 import routes from 'routes'
 
