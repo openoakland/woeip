0bf0d0f0c902dfe95c84ba2802f8e646914a2d52
Luke Travis <luketravis@Lukes-Air.attlocal.net>
2019-11-30 17:58:35 -0800

    lt/add map datepicker, styles, and zoom capabilities

--- web/src/components/Map/index.tsx
+++ web/src/components/Map/index.tsx
@@ -3,19 +3,28 @@ import React, {Component, Fragment} from 'react';
 import ReactMapGL, {Marker} from 'react-map-gl';
 import MapFilters from 'components/Map/ControlPanel'
 
+/*
+  Map component is an extension of the react-map-gl library, which combines react and mapbox.
+  The ReactMapGL component is essentially a reactified mapbox exctraction.
+  Documentation lives here: https://uber.github.io/react-map-gl/#/Documentation/introduction/introduction
+
+  TODO: Data will be populated via componentDidMount and D3, and overlaid onto the map
+*/
+
 const MapContainer = styled.div`
   position: relative;
   margin-top: 40px;
 `
 
-
-class Map extends Component<Viewport> {
+class Map extends Component {
 
   state = {
     viewport: {
       width: "100%",
-      height: 700,
+      height: 600,
       zoom: 14,
+      latitude: 37.812036,
+      longitude: -122.286675
     }
   }
 
@@ -32,10 +41,6 @@ class Map extends Component<Viewport> {
 
   };
 
-  _updateViewport = viewport => {
-
-  };
-
   onMarkerDragEnd = (event) => {
 
   };
@@ -46,8 +51,8 @@ class Map extends Component<Viewport> {
       <MapContainer className="map-container">
         <ReactMapGL
           {...this.state.viewport}
-          latitude={37.812036}
-          longitude={-122.286675}
+          mapStyle='mapbox://styles/mapbox/streets-v11'
+          onViewportChange={(viewport) => this.setState({viewport})}
           mapboxApiAccessToken={'pk.eyJ1IjoibHVrZWh0cmF2aXMiLCJhIjoiY2p2djlmY2sxM3pzcDQzb2p3MXN3aGl2aSJ9.6b2Kp7pfaV-cNiwtYRhRZw'}
         >
           <MapFilters />
