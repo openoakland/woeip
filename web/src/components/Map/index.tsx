5c9761372bb13d033d49be538deecc4cb53a9779
Luke Travis <lukehtravis@gmail.com>
2020-01-14 23:41:00 -0800

    lt/render data points on map

--- web/src/components/Map/index.tsx
+++ web/src/components/Map/index.tsx
@@ -1,7 +1,8 @@
 import styled from 'theme'
 import React, {Component, Fragment} from 'react';
 import ReactMapGL, {Marker} from 'react-map-gl';
-import MapFilters from 'components/Map/ControlPanel'
+import MapFilters from 'components/Map/ControlPanel';
+import Pin from 'components/Map/Pin'
 
 /*
   Map component is an extension of the react-map-gl library, which combines react and mapbox.
@@ -11,6 +12,37 @@ import MapFilters from 'components/Map/ControlPanel'
   TODO: Data will be populated via componentDidMount and D3, and overlaid onto the map
 */
 
+interface IndividualPollutant {
+  time_geo: string,
+  pollutant: string,
+  value: number
+}
+
+interface State {
+  data: Array<PollutionCoordinate>,
+  viewport: Viewport
+}
+
+interface Viewport {
+  width: string,
+  height: number,
+  zoom: number,
+  latitude: number,
+  longitude: number
+}
+
+interface PollutionCoordinate {
+  time: Date,
+  coordinates: Coordinates,
+  pollutant: string,
+  value: number
+}
+
+interface Coordinates {
+  latitude: number,
+  longitude: number
+}
+
 const MapContainer = styled.div`
   position: relative;
   margin-top: 40px;
@@ -18,7 +50,8 @@ const MapContainer = styled.div`
 
 class Map extends Component {
 
-  state = {
+  state: State = {
+    data: [],
     viewport: {
       width: "100%",
       height: 600,
@@ -28,7 +61,14 @@ class Map extends Component {
     }
   }
 
-
+  _renderSpotMarker = (location:PollutionCoordinate) => {
+    return (
+      <Marker longitude={location.coordinates.longitude} latitude={location.coordinates.latitude}>
+        <Pin />
+      </Marker>
+    );
+  }
+/*
   handleClick = (event) => {
 
   };
@@ -44,6 +84,31 @@ class Map extends Component {
   onMarkerDragEnd = (event) => {
 
   };
+*/
+  componentDidMount():void {
+    fetch("http://api.lvh.me/pollutant_values")
+    .then((response) => {
+      return response.json();
+    })
+    .then((pollutionData) => {
+      let pollutantCoordsAndValues = pollutionData.map((individualPollutant: IndividualPollutant, index: number): PollutionCoordinate => {
+        const date = new Date(individualPollutant.time_geo.substr(0, individualPollutant.time_geo.indexOf(' (')).replace(' ', ':'))
+        const startCut:number =  individualPollutant.time_geo.indexOf('(') + 1
+        const endCut: number = individualPollutant.time_geo.indexOf(')')
+        const [latitude, longitude] = individualPollutant.time_geo.slice(startCut, endCut).split(", ")
+        return {
+          time: date,
+          coordinates: {
+            latitude: +latitude,
+            longitude: +longitude,
+          },
+          pollutant: individualPollutant.pollutant,
+          value:  individualPollutant.value
+        }
+      })
+      this.setState({data: pollutantCoordsAndValues})
+    });
+  }
 
   render() {
 
@@ -52,9 +117,17 @@ class Map extends Component {
         <ReactMapGL
           {...this.state.viewport}
           mapStyle='mapbox://styles/mapbox/streets-v11'
-          onViewportChange={(viewport) => this.setState({viewport})}
+          onViewportChange={(viewport: Viewport) => this.setState({viewport})}
           mapboxApiAccessToken={'pk.eyJ1IjoibHVrZWh0cmF2aXMiLCJhIjoiY2p2djlmY2sxM3pzcDQzb2p3MXN3aGl2aSJ9.6b2Kp7pfaV-cNiwtYRhRZw'}
         >
+          {
+            this.state.data.length > 0 && (
+              this.state.data.map(location => {
+                return this._renderSpotMarker(location);
+              })
+            )
+
+          }
           <MapFilters />
         </ReactMapGL>
       </MapContainer>
