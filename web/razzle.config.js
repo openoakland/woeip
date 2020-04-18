28e87baef87dba176c255f2d596e00304967d903
Joshua Ling <joshling1919@gmail.com>
2020-04-18 09:51:37 -0700

    Reset with updated packages

--- web/razzle.config.js
+++ web/razzle.config.js
@@ -1,9 +1,7 @@
-'use strict';
-
-const path = require('path')
+'use strict'
 
 module.exports = {
-  plugins: ['scss', 'typescript'],
+  plugins: ['typescript'],
   modify: (defaultConfig, { target, dev }) => {
     const config = defaultConfig
     if (!dev) {
@@ -19,13 +17,13 @@ module.exports = {
 
     if (config.devServer) {
       // Handle HMR within docker env: https://github.com/jaredpalmer/razzle/issues/416
-      config.devServer.watchOptions['poll'] = 1000;
-      config.devServer.watchOptions['aggregateTimeout'] = 300;
+      config.devServer.watchOptions['poll'] = 1000
+      config.devServer.watchOptions['aggregateTimeout'] = 300
     }
 
-    config.resolve.alias = {	
-      ...config.resolve.alias,	
-      components: path.resolve('./src/components'),	
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      components: path.resolve('./src/components'),
       routes: path.resolve('./src/routes')
     }
 
--- web/src/Home.tsx
+++ web/src/Home.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import logo from './react.svg'
 import { Link } from 'react-router-dom'
-
-import './Home.css'
 
 class Home extends React.Component<{}, {}> {
   public render() {
     return (
       <div className='Home'>
         <div className='Home-header'>
-          <img src={logo} className='Home-logo' alt='logo' />
           <h2>WOAQ</h2>
         </div>
         <ul className='Home-resources'>
