45be3d88cbe952ec750ae54d3fe9fb63fb25f16c
tangoyankee <miller.tim108@gmail.com>
2020-04-23 18:52:22 -0700

    superficial app test

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 import App from './App'
-
 import { MemoryRouter } from 'react-router-dom'
 
 describe('<App />', () => {
   test('renders without exploding', () => {
-    const div = document.createElement('div')
     render(
       <MemoryRouter>
         <App />
-      </MemoryRouter>,
-      div
+      </MemoryRouter>
     )
+    expect(screen.getByRole('heading')).toHaveTextContent('WOAQ')
+    expect(screen.getByRole('list')).toHaveClass('Home-resources')
   })
 })
