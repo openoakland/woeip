5366321d83158abbd38dc23a22d84ba8200141f4
Ajay <ajayvishwanath.94@gmail.com>
2020-05-07 15:04:01 -0700

    Implement Banner

--- web/src/components/Upload_Confirmation/index.tsx
+++ web/src/components/Upload_Confirmation/index.tsx
@@ -4,14 +4,45 @@ import { Message, Icon, Button, Container } from 'semantic-ui-react'
 import styled from 'theme'
 
 const StyledContainer = styled(Container)`
-  display: flex;
-  justify-content: center;
-  padding: 40px;
+  margin-top: 30px;
 `
+
+const ContentContainer = styled.div`
+  width: 100%;
+`
+
+const SuccessBanner = styled(Message)`
+  background: #619e54 !important;
+  height: 48px;
+  width: 100%;
+  display: flex;
+  border-radius: 4px;
+  align-items: center;
+  * {
+    color: ${({ theme }) => theme.colors.white};
+  }
+  i.icon {
+    font-size: 20px;
+  }
+`
+
+const SuccessMessage = styled.span`
+  font-family: ${({ theme }) => theme.fonts.primary};
+  font-size: 16px;
+`
+
+const SuccessIcon = () => <Icon name='check circle outline' />
 
 const UploadConfirmation: React.FunctionComponent = () => {
   return (
-    <StyledContainer>This is the upload confirmation page!</StyledContainer>
+    <StyledContainer>
+      <ContentContainer>
+        <SuccessBanner>
+          <SuccessIcon />
+          <SuccessMessage>Success! Your files were uploaded.</SuccessMessage>
+        </SuccessBanner>
+      </ContentContainer>
+    </StyledContainer>
   )
 }
 
