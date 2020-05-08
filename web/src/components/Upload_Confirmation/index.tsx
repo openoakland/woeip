737bae0fe5ded1f87a6d23837be91b0d4dd51982
Ajay <ajayvishwanath.94@gmail.com>
2020-05-08 16:08:06 -0700

    Complete Basic Styling of Form Elements

--- web/src/components/Upload_Confirmation/index.tsx
+++ web/src/components/Upload_Confirmation/index.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { Message, Icon, Button, Container } from 'semantic-ui-react'
+import {
+  Message,
+  Icon,
+  Button,
+  Container,
+  Form,
+  Select
+} from 'semantic-ui-react'
 
 import styled from 'theme'
 
@@ -8,7 +15,7 @@ const StyledContainer = styled(Container)`
 `
 
 const ContentContainer = styled.div`
-  width: 100%;
+  margin: 0px 125px 0px 125px;
 `
 
 const SuccessBanner = styled(Message)`
@@ -18,20 +25,72 @@ const SuccessBanner = styled(Message)`
   display: flex;
   border-radius: 4px;
   align-items: center;
+  margin-bottom: 32px;
   * {
     color: ${({ theme }) => theme.colors.white};
   }
   i.icon {
     font-size: 20px;
   }
-`
-
-const SuccessMessage = styled.span`
-  font-family: ${({ theme }) => theme.fonts.primary};
-  font-size: 16px;
+  h3 {
+    font-size: 24px;
+  }
 `
 
 const SuccessIcon = () => <Icon name='check circle outline' />
+
+const SuccessMessage = styled.span`
+  font-family: ${({ theme }) => theme.fonts.secondary};
+  font-size: 16px;
+`
+
+const FormContainer = styled.div`
+  width: 500px;
+  margin: 0 auto;
+`
+
+const FormContent = styled(Form)`
+  padding: 22px 72px 80px 32px;
+`
+const DisabledInput = styled(Form.Input)`
+  min-width: auto;
+  width: 160px;
+`
+const DropdownInput = styled(Form.Field)`
+  min-width: auto;
+  width: 160px;
+  * {
+    min-width: auto !important;
+  }
+`
+const SubmitForm = styled.form`
+  display: flex;
+  justify-content: space-between;
+  button {
+    border-radius: 2px !important;
+    font-family: ${({ theme }) => theme.fonts.secondary} !important;
+    font-style: normal !important;
+    font-weight: 500 !important;
+    width: 160px !important;
+    font-size: 16px !important;
+  }
+`
+
+const SaveButton = styled(Button)`
+  color: ${({ theme }) => theme.colors.white} !important;
+  background-color: rgba(53, 53, 53, 0.9) !important;
+`
+
+const CancelButton = styled(Button)`
+  color: rgba(53, 53, 53, 0.9) !important;
+  background-color: #f4f5f4 !important;
+`
+
+const options = [
+  { key: 'm', text: 'Male', value: 'male' },
+  { key: 'f', text: 'Female', value: 'female' },
+  { key: 'o', text: 'Other', value: 'other' }
+]
 
 const UploadConfirmation: React.FunctionComponent = () => {
   return (
@@ -41,6 +100,25 @@ const UploadConfirmation: React.FunctionComponent = () => {
           <SuccessIcon />
           <SuccessMessage>Success! Your files were uploaded.</SuccessMessage>
         </SuccessBanner>
+        <h3>Step 2. Confirm your session details</h3>
+        <FormContainer>
+          <FormContent>
+            <DisabledInput placeholder='First Name' readOnly />
+            <p>Collection Date</p>
+            <DisabledInput placeholder='First Name' readOnly />
+            <p>Start Time</p>
+            <DropdownInput
+              control={Select}
+              options={options}
+              placeholder='Gender'
+            />
+            <p>Device</p>
+            <SubmitForm>
+              <SaveButton>Save</SaveButton>
+              <CancelButton>Cancel</CancelButton>
+            </SubmitForm>
+          </FormContent>
+        </FormContainer>
       </ContentContainer>
     </StyledContainer>
   )
