bf1fa9349658e7077312ed4e99e006b8b3438cfe
tangoyankee <miller.tim108@gmail.com>
2020-08-02 12:45:39 -0700

    move all elements for upload confirmation

--- web/src/components/Upload/UploadConfirmation/elements.tsx
+++ web/src/components/Upload/UploadConfirmation/elements.tsx
@@ -6,8 +6,9 @@ import {
     Input,
     Dropdown
 } from 'semantic-ui-react'
-
 import styled from 'theme'
+
+export const SuccessIcon = () => <Icon name='check circle outline' />
 
 export const StyledContainer = styled(Container)`
   margin-top: 30px;
@@ -16,3 +17,82 @@ export const StyledContainer = styled(Container)`
 export const ContentContainer = styled.div`
   margin: 0px 130px 0px 130px;
 `
+
+export const SuccessBanner = styled(Message)`
+  background: #619e54 !important;
+  height: 48px;
+  width: 100%;
+  display: flex;
+  border-radius: 4px;
+  align-items: center;
+  margin-bottom: 32px;
+  * {
+    color: ${({ theme }) => theme.colors.white};
+  }
+  i.icon {
+    font-size: 20px;
+  }
+  h3 {
+    font-size: 24px;
+  }
+`
+
+export const SuccessMessage = styled.span`
+  font-family: ${({ theme }) => theme.fonts.secondary};
+  font-size: 1rem;
+`
+
+export const FormMessage = styled.h3`
+  font-size: 1.5rem;
+`
+
+export const FormContainer = styled.div`
+  width: 500px;
+  margin: 0 auto;
+`
+
+export const FormContent = styled.div`
+  padding: 1.85rem 4.5rem 5rem 2rem;
+`
+export const DisabledInput = styled(Input)`
+  min-width: auto;
+  width: 160px;
+`
+
+export const DropdownInput = styled(Dropdown)`
+  min-width: auto !important;
+  width: 160px;
+  //eventually need to make borders of this dropdown #232735 per wireframe
+  // :focus-within {
+  //   border: 1px solid #232735 !important;
+  // }
+`
+
+export const InputLabel = styled.p`
+  font-family: ${({ theme }) => theme.fonts.primary};
+  font-size .875rem;
+  line-height: 24px;
+  margin: 0px 0px 32px 0px !important;
+`
+export const SubmitForm = styled.form`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 48px;
+  button {
+    border-radius: 2px !important;
+    font-family: ${({ theme }) => theme.fonts.secondary} !important;
+    font-style: normal !important;
+    font-weight: 500 !important;
+    width: 160px !important;
+    font-size: 1rem !important;
+  }
+`
+
+export const SaveButton = styled(Button)`
+  color: ${({ theme }) => theme.colors.white} !important;
+  background-color: rgba(53, 53, 53, 0.9) !important;
+`
+
+export const CancelButton = styled(Button)`
+  color: rgba(53, 53, 53, 0.9) !important;
+  background-color: #f4f5f4 !important;
--- web/src/components/Upload/UploadConfirmation/index.tsx
+++ web/src/components/Upload/UploadConfirmation/index.tsx
@@ -1,14 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { FileWithPath } from 'react-dropzone'
 import { useHistory } from 'react-router-dom'
-import {
-  Message,
-  Icon,
-  Button,
-  Container,
-  Input,
-  Dropdown
-} from 'semantic-ui-react'
 import * as confirmationElements from 'components/Upload/UploadConfirmation/elements'
 import {
   identFiles,
@@ -16,100 +8,7 @@ import {
   getDustrakEnd
 } from 'components/Upload/util'
 import axios from 'axios'
-import styled from 'theme'
 import moment from 'moment-timezone'
-
-// const StyledContainer = styled(Container)`
-//   margin-top: 30px;
-// `
-
-// const ContentContainer = styled.div`
-//   margin: 0px 130px 0px 130px;
-// `
-
-const SuccessBanner = styled(Message)`
-  background: #619e54 !important;
-  height: 48px;
-  width: 100%;
-  display: flex;
-  border-radius: 4px;
-  align-items: center;
-  margin-bottom: 32px;
-  * {
-    color: ${({ theme }) => theme.colors.white};
-  }
-  i.icon {
-    font-size: 20px;
-  }
-  h3 {
-    font-size: 24px;
-  }
-`
-
-const SuccessIcon = () => <Icon name='check circle outline' />
-
-const SuccessMessage = styled.span`
-  font-family: ${({ theme }) => theme.fonts.secondary};
-  font-size: 1rem;
-`
-
-const FormMessage = styled.h3`
-  font-size: 1.5rem;
-`
-
-const FormContainer = styled.div`
-  width: 500px;
-  margin: 0 auto;
-`
-
-const FormContent = styled.div`
-  padding: 1.85rem 4.5rem 5rem 2rem;
-`
-const DisabledInput = styled(Input)`
-  min-width: auto;
-  width: 160px;
-`
-
-// const CalendarIcon = () => <Icon name='calendar outline' />
-
-const DropdownInput = styled(Dropdown)`
-  min-width: auto !important;
-  width: 160px;
-  //eventually need to make borders of this dropdown #232735 per wireframe
-  // :focus-within {
-  //   border: 1px solid #232735 !important;
-  // }
-`
-
-const InputLabel = styled.p`
-  font-family: ${({ theme }) => theme.fonts.primary};
-  font-size .875rem;
-  line-height: 24px;
-  margin: 0px 0px 32px 0px !important;
-`
-const SubmitForm = styled.form`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 48px;
-  button {
-    border-radius: 2px !important;
-    font-family: ${({ theme }) => theme.fonts.secondary} !important;
-    font-style: normal !important;
-    font-weight: 500 !important;
-    width: 160px !important;
-    font-size: 1rem !important;
-  }
-`
-
-const SaveButton = styled(Button)`
-  color: ${({ theme }) => theme.colors.white} !important;
-  background-color: rgba(53, 53, 53, 0.9) !important;
-`
-
-const CancelButton = styled(Button)`
-  color: rgba(53, 53, 53, 0.9) !important;
-  background-color: #f4f5f4 !important;
-`
 
 const options = [
   { key: 'a', text: 'Device A', value: 'Device A' },
@@ -175,39 +74,39 @@ const UploadConfirmation: React.FunctionComponent<Array<
   return (
     <confirmationElements.StyledContainer>
       <confirmationElements.ContentContainer>
-        <SuccessBanner>
-          <SuccessIcon />
-          <SuccessMessage>Success! Your files were uploaded.</SuccessMessage>
-        </SuccessBanner>
-        <FormMessage>Step 2. Confirm your session details</FormMessage>
-        <FormContainer>
-          <FormContent>
+        <confirmationElements.SuccessBanner>
+          <confirmationElements.SuccessIcon />
+          <confirmationElements.SuccessMessage>Success! Your files were uploaded.</confirmationElements.SuccessMessage>
+        </confirmationElements.SuccessBanner>
+        <confirmationElements.FormMessage>Step 2. Confirm your session details</confirmationElements.FormMessage>
+        <confirmationElements.FormContainer>
+          <confirmationElements.FormContent>
             <div>
-              <DisabledInput
+              <confirmationElements.DisabledInput
                 value={dustrakStart.format('MM/DD/YYYY')}
                 icon='calendar outline'
                 disabled={true}
               />
             </div>
-            <InputLabel>Collection date</InputLabel>
-            <DisabledInput
+            <confirmationElements.InputLabel>Collection date</confirmationElements.InputLabel>
+            <confirmationElements.DisabledInput
               value={dustrakStart.format('h:mm A')}
               disabled={true}
             />
-            <InputLabel>Start time</InputLabel>
-            <DropdownInput
+            <confirmationElements.InputLabel>Start time</confirmationElements.InputLabel>
+            <confirmationElements.DropdownInput
               search={true}
               selection={true}
               options={options}
               defaultValue={options[0].value}
             />
-            <InputLabel>Device</InputLabel>
-            <SubmitForm onSubmit={upload}>
-              <SaveButton type='submit'>Save</SaveButton>
-              <CancelButton onClick={cancelUpload}>Cancel</CancelButton>
-            </SubmitForm>
-          </FormContent>
-        </FormContainer>
+            <confirmationElements.InputLabel>Device</confirmationElements.InputLabel>
+            <confirmationElements.SubmitForm onSubmit={upload}>
+              <confirmationElements.SaveButton type='submit'>Save</confirmationElements.SaveButton>
+              <confirmationElements.CancelButton onClick={cancelUpload}>Cancel</confirmationElements.CancelButton>
+            </confirmationElements.SubmitForm>
+          </confirmationElements.FormContent>
+        </confirmationElements.FormContainer>
       </confirmationElements.ContentContainer>
     </confirmationElements.StyledContainer>
   )
