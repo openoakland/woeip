ed77ff117fcf6651ef5b248aa77643ae5f3a6a77
tangoyankee <miller.tim108@gmail.com>
2020-08-12 16:12:44 -0700

    absolute footer

--- web/src/components/App/index.tsx
+++ web/src/components/App/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'emotion-theming'
-import { theme } from 'theme'
+import styled, { theme } from 'theme'
 import Navbar from 'components/Navbar'
 import Footer from 'components/Footer'
 
@@ -10,14 +10,20 @@ import routes from 'routes'
 import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 
+const Content = styled.div`
+  padding-bottom: 124px;
+`
+
 const App = () => (
   <ThemeProvider theme={theme}>
     <Navbar />
+    <Content>
     <Switch>
       {routes.map(route => (
         <Route key={route.path} {...route} />
       ))}
     </Switch>
+    </Content>
     <Footer />
   </ThemeProvider>
 )
--- web/src/components/Footer/elements.tsx
+++ web/src/components/Footer/elements.tsx
@@ -4,16 +4,13 @@ export const Container = styled.div`
   position: absolute;
   bottom: 0;
   left: 0;
-  margin: auto;
   width: 100%;
   background-color: ${({ theme }) => theme.colors.gray};
 `
 
 export const LinkList = styled.ul`
   list-style: none;
-  padding-top: 32px;
-  padding-bottom: 32px;
-  padding-left: 40px;
+  padding: 32px 40px;
 `
 
 export const ExternalLink = styled.a`
