ed682915f5a7528306f7c0686b11328c45b38660
tangoyankee <miller.tim108@gmail.com>
2020-08-12 13:36:19 -0700

    lint

--- web/src/components/Footer/index.tsx
+++ web/src/components/Footer/index.tsx
@@ -1,21 +1,23 @@
 import * as React from 'react'
 import { FooterLink } from 'components/Footer/types'
 
-const footerLinks: FooterLink[] = [
-    { name: 'WOAQ on GitHub', url: 'https://github.com/openoakland/woeip' },
-    { name: 'OpenOakland', url: 'https://openoakland.org' },
-    { name: 'WOEIP', url: 'https://woeip.org' }
+const footerLinks: Array<FooterLink> = [
+  { name: 'WOAQ on GitHub', url: 'https://github.com/openoakland/woeip' },
+  { name: 'OpenOakland', url: 'https://openoakland.org' },
+  { name: 'WOEIP', url: 'https://woeip.org' }
 ]
 
 const Footer: React.FunctionComponent = () => {
-    const footerItems = footerLinks.map((link: FooterLink) => (
-        <li key={link.name}>
-            <a href={link.url}>{link.name}</a>
-        </li>
-    ))
-    return <div>
-        <ul>{footerItems}</ul>
+  const footerItems = footerLinks.map((link: FooterLink) => (
+    <li key={link.name}>
+      <a href={link.url}>{link.name}</a>
+    </li>
+  ))
+  return (
+    <div>
+      <ul>{footerItems}</ul>
     </div>
+  )
 }
 
 export default Footer
