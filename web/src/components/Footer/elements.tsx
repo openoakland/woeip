0efe5c5b64223ef91d897b31b47f4c645dc03533
tangoyankee <miller.tim108@gmail.com>
2020-08-12 14:55:12 -0700

    pin footer to bottom

--- web/src/components/Footer/elements.tsx
+++ web/src/components/Footer/elements.tsx
@@ -1,11 +1,19 @@
 import styled from 'theme'
 
 export const Container = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  width: 100%;
   background-color: ${({ theme }) => theme.colors.gray};
 `
 
 export const LinkList = styled.ul`
   list-style: none;
+  padding-top: 32px;
+  padding-bottom: 32px;
+  padding-left: 40px;
 `
 
 export const ExternalLink = styled.a`
