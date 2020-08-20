bf8cf7590e2f9f144383b1025085a0219980ed60
Ajay <ajayvishwanath.94@gmail.com>
2020-08-19 21:03:46 -0700

    Implement Datepicker Functionality

--- web/src/components/Map/ControlPanelAjay/index.tsx
+++ web/src/components/Map/ControlPanelAjay/index.tsx
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'theme'
 import SemanticDatepicker from 'react-semantic-ui-datepickers'
 import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css'
+import moment from 'moment-timezone'
+
+const FormMessage = styled.h3`
+  font-size: 1.5rem;
+`
 
 const ControlPanel: React.FunctionComponent = () => {
+  const [date, setDate] = useState<moment.Moment>(moment())
+
+  const onChange = (event, data) => {
+    const parsed = moment(data.value)
+    setDate(parsed)
+  }
+
   return (
     <div>
-      <p>this is the control panel</p>
-      <SemanticDatepicker />
+      <FormMessage>{date.format('LL')}</FormMessage>
+      <SemanticDatepicker
+        onChange={onChange}
+        format='MM/DD/YYYY'
+        value={date.toDate()}
+        clearable={false}
+      />
     </div>
   )
 }
