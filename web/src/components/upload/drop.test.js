a29e92d1949909bcb3630857987cc405827dcdbc
Timothy Miller <miller.tim108@gmail.com>
2021-03-10 20:23:31 -0500

    finish tests for drop react component

--- web/src/components/upload/drop.test.js
+++ web/src/components/upload/drop.test.js
@@ -1,8 +1,65 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent, waitFor } from "@testing-library/react";
 import fs from "fs";
 import { UploadDrop } from "./drop";
 
-describe("Upload Drop", () => {
+describe("Actions after extracting files", () => {
+  let validGps;
+  let validDustrak;
+  let wrongDustrak;
+
+  let csvFileValid;
+  let csvFileWrong;
+  let logFile;
+  /**
+   * It is time-intensive to extract files.
+   * Run the process exactly once and make the data available to the whole test block
+   * Any test suites that rely on file data are placed in this test block
+   */
+  const testDir = `${__dirname}/test-data`;
+  beforeAll(async () => {
+    const readFile = (path, opts = "utf8") =>
+      new Promise((resolve, reject) => {
+        fs.readFile(path, opts, (err, data) => {
+          if (err) reject(`error: ${err}`);
+          else resolve(data.toString());
+        });
+      });
+
+    const fileNames = ["valid.log", "valid.csv", "wrong.csv"];
+    const filePromises = [];
+    for (const file of fileNames) {
+      const filePomise = readFile(`${testDir}/${file}`);
+      filePromises.push(filePomise);
+    }
+
+    [validGps, validDustrak, wrongDustrak] = await Promise.all(filePromises);
+
+    csvFileValid = new File(["file"], "file.csv", {
+      type: "text/csv",
+    });
+    csvFileValid.text = async () => validDustrak;
+
+    csvFileWrong = new File(["csvFileWrong"], "csvFileWrong.csv", {
+      type: "text/csv",
+    });
+    csvFileWrong.text = async () => wrongDustrak;
+
+    logFile = new File(["file"], "file.log", {
+      type: "text/log",
+    });
+    logFile.text = async () => validGps;
+  });
+
+  afterAll(() => {
+    validGps = null;
+    validDustrak = null;
+    wrongDustrak = null;
+
+    csvFileValid = null;
+    csvFileWrong = null;
+    logFile = null;
+  });
+
   it("should have a drop zone and no pending files", () => {
     renderUploadDrop();
     expect(screen.getByText(/DusTrak and GPS files/i)).toBeInTheDocument();
@@ -23,118 +80,48 @@ describe("Upload Drop", () => {
   });
 
   it("should have an error message for three files", () => {
-    const files = [
-      new File(["hello"], "hello.csv", { type: "text/csv" }),
-      new File(["hello again"], "hello again.csv", { type: "text/csv" }),
-      new File(["goodbye"], "goodbye.log", { type: "text/log" }),
-    ];
+    const files = [csvFileValid, csvFileValid, logFile];
     renderUploadDrop({ files });
     expect(screen.getByText(/remove additional files/)).toBeInTheDocument();
   });
 
   it("should have an error message for two csv files", () => {
-    const files = [
-      new File(["hello"], "hello.csv", { type: "text/csv" }),
-      new File(["hello again"], "hello again.csv", { type: "text/csv" }),
-    ];
+    const files = [csvFileValid, csvFileValid];
     renderUploadDrop({ files });
     expect(screen.getByText(/replace one of your files/)).toBeInTheDocument();
   });
 
-  describe("Errors from extracting Files", () => {
-    let validGps;
-    let gprmcMissing;
-    let gprmcCorrupt;
-    let validDustrak;
-    let wrongDustrak;
-    let missingStartDustrak;
-    let empty;
-    /**
-     * It is time-intensive to extract files.
-     * Run the process exactly once and make the data available to the whole test block
-     * Any test suites that rely on file data are placed in this test block
-     */
-    const testDir = `${__dirname}/test-data`;
-    beforeAll(async () => {
-      const readFile = (path, opts = "utf8") =>
-        new Promise((resolve, reject) => {
-          fs.readFile(path, opts, (err, data) => {
-            if (err) reject(`error: ${err}`);
-            else resolve(data.toString().split("\n", 10));
-          });
-        });
-
-      const fileNames = [
-        "valid.log",
-        "gprmc_missing.log",
-        "gprmc_corrupt.log",
-        "valid.csv",
-        "wrong.csv",
-        "start_missing.csv",
-        "empty.txt",
-      ];
-      const filePromises = [];
-      for (const file of fileNames) {
-        const filePomise = readFile(`${testDir}/${file}`);
-        filePromises.push(filePomise);
-      }
-
-      [
-        validGps,
-        gprmcMissing,
-        gprmcCorrupt,
-        validDustrak,
-        wrongDustrak,
-        missingStartDustrak,
-        empty,
-      ] = await Promise.all(filePromises);
-    });
-
-    afterAll(() => {
-      validGps = null;
-      gprmcMissing = null;
-      gprmcCorrupt = null;
-      validDustrak = null;
-      wrongDustrak = null;
-      missingStartDustrak = null;
-      empty = null;
-    });
-
-    it.skip("should have no error message for gps and dustrak pair", () => {
-      const files = [
-        new File(["hello"], "hello.csv", {
-          type: "text/csv",
-          text: async () => validDustrak.join("\n"),
-        }),
-        new File(["goodbye"], "goodbye.log", {
-          type: "text/log",
-          text: async () => validGps.join("\n"),
-        }),
-      ];
-      renderUploadDrop({ files });
-      expect(screen.queryByText(/Please/)).not.toBeInTheDocument();
-    });
-
-    it.skip("should have an error message for gps and dustrak pair", () => {
-      const files = [
-        new File(["hello"], "hello.csv", {
-          type: "text/csv",
-          text: async () => wrongDustrak.join("\n"),
-        }),
-        new File(["goodbye"], "goodbye.log", {
-          type: "text/log",
-          text: async () => validGps.join("\n"),
-        }),
-      ];
-      renderUploadDrop({ files });
+  it("should have no error message for gps and dustrak pair", async () => {
+    renderUploadDrop({ files: [csvFileValid, logFile] });
+    await waitFor(() => {
+      expect(screen.getByText(/Pending Files/)).toBeInTheDocument();
       expect(screen.queryByText(/Please/)).not.toBeInTheDocument();
     });
   });
 
-  // May need to be an integration test placed on index test
-  it.todo("should remove a file when click a trash icon");
-  // Integration test on index
-  it.todo("should proceed to the confirm page when there are two valid files");
+  it("should have an error message for gps and dustrak pair", async () => {
+    renderUploadDrop({ files: [csvFileWrong, logFile] });
+    await waitFor(() =>
+      expect(screen.getByText(/Try again/)).toBeInTheDocument()
+    );
+  });
+
+  it("should remove a file when click a trash icon", async () => {
+    const files = [csvFileValid, csvFileWrong, logFile];
+    const setFiles = jest.fn();
+    renderUploadDrop({ files, setFiles });
+    const trashButtons = screen.getAllByRole("button");
+    expect(trashButtons).toHaveLength(3);
+    fireEvent.click(trashButtons[0]);
+    expect(setFiles).toHaveBeenCalledWith([files[1], files[2]]);
+  });
+
+  it("should proceed to the confirm page when there are two valid files", async () => {
+    const files = [csvFileValid, logFile];
+    const proceedToConfirm = jest.fn();
+    renderUploadDrop({ files, proceedToConfirm });
+    expect(proceedToConfirm).toHaveBeenCalled();
+  });
 });
 
 const renderUploadDrop = ({
@@ -143,7 +130,7 @@ const renderUploadDrop = ({
   proceedToConfirm = jest.fn(),
   setDustrakStart = jest.fn(),
   setDustrakEnd = jest.fn(),
-  setGpsStart = jest.fn(),
+  setDustrakSerial = jest.fn(),
 } = {}) =>
   render(
     <UploadDrop
@@ -153,7 +140,7 @@ const renderUploadDrop = ({
         proceedToConfirm,
         setDustrakStart,
         setDustrakEnd,
-        setGpsStart,
+        setDustrakSerial,
       }}
     />
   );
