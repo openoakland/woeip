e522aabba09a1606901956e954d718b1ccd2b527
Kabirdas Henry <52506986+kbrdsk@users.noreply.github.com>
2021-07-04 21:01:37 -0700

    Update web/src/components/nav/betaNotice.js
    Co-authored-by: Timothy Miller <miller.tim108@gmail.com>

--- web/src/components/nav/betaNotice.js
+++ web/src/components/nav/betaNotice.js
@@ -2,7 +2,7 @@ import { Route } from "react-router-dom";
 import { DismissableMessage } from "../ui";
 import "./betaNotice.css";
 
-const MESSAGECONTENT =
+const MESSAGE_CONTENT =
 	"WOAQ is currently in early release. You may encounter bugs and data may not be displayed accurately.";
 
 export const BetaNotice = () => (
