08614d45cea194ab69855924c710c655886605ef
Kabirdas Henry <kbrdsk@gmail.com>
2021-07-04 20:50:48 -0700

    move css to separate file

--- web/src/components/nav/betaNotice.js
+++ web/src/components/nav/betaNotice.js
@@ -1,20 +1,12 @@
 import { Route } from "react-router-dom";
 import { DismissableMessage } from "../ui";
-
-const MESSAGESTYLE = {
-	background: "#fec61e",
-	marginTop: "1em",
-	display: "inline-block",
-	width: "100%",
-};
+import "./betaNotice.css";
 
 const MESSAGECONTENT =
 	"WOAQ is currently in early release. You may encounter bugs and data may not be displayed accurately.";
 
-export const BetaNotice = () => {
-	return (
-		<Route path={["/maps", "/upload"]}>
-			<DismissableMessage style={MESSAGESTYLE} content={MESSAGECONTENT} />
-		</Route>
-	);
-};
+export const BetaNotice = () => (
+	<Route path={["/maps", "/upload"]}>
+		<DismissableMessage className="beta-notice" content={MESSAGECONTENT} />
+	</Route>
+);
