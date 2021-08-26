c86e8de144b5df147a4b5f04a5dcefcba0325cbe
Wendy Wu <wendywu@gitlab.com>
2021-08-26 12:47:09 -0700

    add new libraries and change user files

--- api/woeip/apps/core/admin.py
+++ api/woeip/apps/core/admin.py
@@ -3,5 +3,4 @@ from django.contrib.auth.admin import UserAdmin
 from woeip.apps.core.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-
 admin.site.register(User, UserAdmin)
