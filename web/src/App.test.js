7d3a48831cecd481650dd6583281ef7a0d279410
Timothy Miller <miller.tim108@gmail.com>
2021-08-29 10:19:47 -0400

    remove unused mock

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -4,20 +4,11 @@ import { App } from "./App";
 /**
 * gl-js used in "box" component has hard requirements for browser apis. it cannot run in jest
 * https://github.com/mapbox/mapbox-gl-js/issues/10487#issuecomment-805106543
+*
+*
+* Alternative fix at https://github.com/mapbox/mapbox-gl-js/issues/3436#issuecomment-485535598
 */
 jest.mock("./components/map/box", () => () => <></>);
-
-// https://github.com/mapbox/mapbox-gl-js/issues/3436
-jest.mock("mapbox-gl/dist/mapbox-gl", () => ({
-  GeolocateControl: jest.fn(),
-  Map: jest.fn(() => ({
-    addControl: jest.fn(),
-    on: jest.fn(),
-    remove: jest.fn(),
-    workerClass: jest.fn(),
-  })),
-  NavigationControl: jest.fn(),
-}));
 
 describe('', () => {
   it('should have part of the header', () => {
