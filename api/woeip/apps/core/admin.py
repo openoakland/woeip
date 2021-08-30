4ce062b92a595a2cca4ca10a9c19f7fb745ce847
Alana Tran <alanatran@Hues-MacBook-Pro.local>
2021-08-30 00:54:28 -0700

    admin and user fields align; no more bug but fields need modifying

--- api/woeip/apps/core/admin.py
+++ api/woeip/apps/core/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from woeip.apps.core.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+class UserAdmin(BaseUserAdmin):
+    # add_form =  UserCreationForm
+
+    list_display = ('name', 'email', 'is_staff')
+    list_filter = ('is_staff',)
+
+    fieldsets = (
+        (None, {'fields': ('name','email','password')}),
+
+        ('Permissions', {'fields': ('is_staff',)}),
+    )
+
+    search_fields =  ('name', 'email')
+    ordering = ('name','email')
+
+    filter_horizontal = ()
 
 admin.site.register(User, UserAdmin)
