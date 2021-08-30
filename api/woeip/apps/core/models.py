777db75ed6792eecd4480af0fc022d5747c26457
Alana Tran <alanatran@Hues-MacBook-Pro.local>
2021-08-30 00:54:28 -0700

    admin and user fields align; no more bug but fields need modifying

--- api/woeip/apps/core/models.py
+++ api/woeip/apps/core/models.py
@@ -15,40 +15,6 @@ class UserAccountManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
-    # first_name = models.CharField(
-    #     _('first name'),
-    #     max_length=30,
-    #     blank=True,
-    # )
-    # last_name = models.CharField(
-    #     _('last name'),
-    #     max_length=150,
-    #     blank=True,
-    # )
-    # is_staff = models.BooleanField(
-    #     _('staff status'),
-    #     default=False,
-    #     help_text=_(
-    #         'Designates whether the user can log into '
-    #         'this admin site.'
-    #     ),
-    # )
-    # is_active = models.BooleanField(
-    #     _('active'),
-    #     default=True,
-    #     help_text=_(
-    #         'Designates whether this user should be '
-    #         'treated as active. Unselect this instead '
-    #         'of deleting accounts.'
-    #     ),
-    # )
-    # date_joined = models.DateTimeField(
-    #     _('date joined'),
-    #     default=timezone.now,
-    # )
-
-    # # Additional fields
-
     objects = UserAccountManager()
     USERNAME_FIELD = 'email'
 
