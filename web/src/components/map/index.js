34b071a2c1b47bf0dccf1800e1e348844c1fe32f
Timothy Miller <miller.tim108@gmail.com>
2021-09-02 20:43:17 -0400

    get pollutants

--- web/src/components/map/index.js
+++ web/src/components/map/index.js
@@ -1,64 +1,154 @@
-import { useEffect, useState } from "react"
-import { useLocation } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import moment from "moment-timezone";
 import axios from "axios";
-import { apiUrlCollections } from "../../api.util";
+import { apiUrlCollectionById, apiUrlCollections } from "../../api.util";
 
 export const Map = () => {
-    const location = useLocation(); // location for the url
-    // const initialDate = moment(location?.state?.date) || moment();
-    const initialDate = moment("07/23/2014") || moment();
-    const [mapDate, setMapDate] = useState(initialDate);
-    const [collectionsOnDate, setCollectionsOnDate] = useState([]);
-    const [collectionsTokenSource, setCollectionsTokenSource]  = useState(axios.CancelToken.source());
-    const [pollutants, setPollutants] = useState([]);
-    const [pollutantsTokenSource, setPollutantsTokenSource] = useState(axios.CancelToken.source());
+  const location = useLocation(); // location for the url
+  // const initialDate = moment(location?.state?.date) || moment();
+  const initialDate = moment("07/23/2014") || moment();
+  const [activeCollection, setActiveCollection] = useState({});
+  const [mapDate, setMapDate] = useState(initialDate);
+  const [collectionsOnDate, setCollectionsOnDate] = useState([]);
+  const [collectionsTokenSource, setCollectionsTokenSource] = useState(
+    axios.CancelToken.source()
+  );
+  const [pollutants, setPollutants] = useState([]);
+  const [isPendingResponse, setIsPendingResponse] = useState(true);
+  const [pollutantsTokenSource, setPollutantsTokenSource] = useState(
+    axios.CancelToken.source()
+  );
+  const [errorMessage, setErrorMessage] = useState("");
 
-    useEffect(() => {
-        console.log('once on mount!');
-        console.log(mapDate);
-        console.log("start date", location?.state?.date);
-        // get collections on date
-        (async () => {
-            const options = {
-                params: {
-                    start_date: mapDate.format("YYYY-MM-DD"),
-                },
-                cancelToken: collectionsTokenSource.source,
-            };
-            console.log('collectionsTokenSource', collectionsTokenSource);
-            const rawCollectionsOnDate = await axios.get(apiUrlCollections(), options);
-            const collectionsOnDateData = rawCollectionsOnDate.data
-            if(collectionsOnDateData){
-                console.log('collectionsOnDateData', collectionsOnDateData);
-                setCollectionsOnDate(collectionsOnDateData);
-                const activeCollection = collectionsOnDateData[collectionsOnDateData.length - 1]
+  useEffect(() => {
+    // console.log('once on mount!');
+    // console.log(mapDate);
+    // console.log("start date", location?.state?.date);
+    (async () => {
+      const options = {
+        params: {
+          start_date: mapDate.format("YYYY-MM-DD"),
+        },
+        cancelToken: collectionsTokenSource.source,
+      };
+      console.log("collectionsTokenSource", collectionsTokenSource);
+      // Get the collections on the day
+      let rawCollectionsOnDate;
+      try {
+        rawCollectionsOnDate = await axios.get(apiUrlCollections(), options);
+      } catch {
+        resetData();
+        setErrorMessage("error fetching collections");
+        return;
+      }
+      const collectionsOnDateData = rawCollectionsOnDate.data;
+      if (!collectionsOnDateData) {
+        resetData();
+        setErrorMessage("error reading collection data");
+        return;
+      }
 
-            } else {
-                setCollectionsOnDate([]);
-                setPollutants([]);
-            };
-            console.log('rawCollectionsOnDate', (rawCollectionsOnDate));
-            // setCollectionsOnDate
-        })();
-        return () => {
-            collectionsTokenSource.cancel();
-            pollutantsTokenSource.cancel();
-        };
-    }, [])
+      if (!collectionsOnDateData.length) {
+        resetData();
+        setErrorMessage("error no collections found on date");
+        return;
+      } else {
+        setCollectionsOnDate(collectionsOnDateData);
+        const activeCollection =
+          collectionsOnDateData[collectionsOnDateData.length - 1];
+        if (!activeCollection.id) {
+          resetData();
+          setErrorMessage("error no collection id");
+          return;
+        }
+        setActiveCollection(activeCollection);
+        // get the pollutants from the first collection of the day
+        let rawPollutantsOnDate;
+        try {
+          const options = {
+            cancelToken: pollutantsTokenSource.token,
+          };
+          rawPollutantsOnDate = await axios.get(
+            apiUrlCollectionById(activeCollection.id),
+            options
+          );
+        } catch {
+          resetData();
+          setErrorMessage("error fetching pollutant data");
+        }
+        const rawPollutantsOnDateData = rawPollutantsOnDate.data;
+        if (!rawPollutantsOnDateData) {
+          // no need to reset the collections data under this circumstance.
+          // they were already validated.
+          resetData();
+          setErrorMessage("error no pollutants");
+          return;
+        } else {
+          setPollutants(rawPollutantsOnDateData);
+        }
+      }
+      // Conditions
+      // There are no data
+      // There are no collections
+      // There are collections and data
+      if (collectionsOnDateData) {
+        console.log("collectionsOnDateData", collectionsOnDateData);
+        setCollectionsOnDate(collectionsOnDateData);
+        const activeCollection =
+          collectionsOnDateData[collectionsOnDateData.length - 1];
+        // get the pollutants from the day
+        // try {
+        //     const options = {
+        //         cancelToken: pollutantsTokenSource.token
+        //     }
+        //     // activeCollection.id &&
+        // } catch {
 
-    return(
-        <div>Hello, Map Data!
-            <button onClick={() => collectionsTokenSource.cancel()}>Cancel Collection Call</button>
-            <p>Collections on Date:</p>
-            {collectionsOnDate.map((collection) => {
+        // }
+
+        // set the active collection to the present one
+      } else {
+        setCollectionsOnDate([]);
+        setPollutants([]);
+      }
+      console.log("rawCollectionsOnDate", rawCollectionsOnDate);
+      // setCollectionsOnDate
+    })();
+    return () => {
+      collectionsTokenSource.cancel();
+      pollutantsTokenSource.cancel();
+    };
+  }, []);
+
+  const resetData = () => {
+    setIsPendingResponse(false);
+    setCollectionsOnDate([]);
+    setCollectionsTokenSource(axios.CancelToken.source());
+    setPollutants([]);
+    setPollutantsTokenSource(axios.CancelToken.source());
+  };
+
+  // TODO: When picking a new date, reset all of the data?
+
+  return (
+    <div>
+      Hello, Map Data!
+      <p>Error: {errorMessage}</p>
+      <p>Is pending reponse: {isPendingResponse ? "yes" : "no"}</p>
+      <button onClick={() => collectionsTokenSource.cancel()}>
+        Cancel Collection Call
+      </button>
+      <p>Count of Collections on Date: {collectionsOnDate.length}</p>
+      <p>Count of pollutants on Data: {pollutants.length}</p>
+      {/* {collectionsOnDate.map((collection) => {
                 return(
                     <div>
                         <p>A collection</p>
                         <p>{collection.id}</p>
                     </div>
                 )
-            })}
-        </div>
-    )
-}
+            })} */}
+    </div>
+  );
+};
--- web/src/components/map/index.test.js
+++ web/src/components/map/index.test.js
@@ -1,10 +1,8 @@
-import Map from '.';
-import { render, screen } from '@testing-library/react';
+import Map from ".";
+import { render, screen } from "@testing-library/react";
 
-
-describe('should load pollutant data', () => {
-    it.todo('should successfully get full pollutant data');
-    it.todo('should successfully get empty pollutant data');
-    it.todo('should handle error when fetching pollutant data');
-})
-
+describe("should load pollutant data", () => {
+  it.todo("should successfully get full pollutant data");
+  it.todo("should successfully get empty pollutant data");
+  it.todo("should handle error when fetching pollutant data");
+});
