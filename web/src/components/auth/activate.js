83a2c2c324dbd0251eb3a75f527581e243d1de08
Alana Tran <alanatran@Hues-MacBook-Pro.local>
2021-09-20 20:59:13 -0700

    beginning work on password pages

--- web/src/components/auth/activate.js
+++ web/src/components/auth/activate.js
@@ -1,3 +1,11 @@
+import { Form } from "semantic-ui-react";
+
 export const Activate = () => {
-  return <div className="activate-container"></div>;
+  return <div className="activate-container">
+    <Form>
+      <Form.Field>
+        
+      </Form.Field>
+    </Form>
+  </div>;
 };
--- web/src/components/auth/resetPassword.js
+++ web/src/components/auth/resetPassword.js
@@ -1,3 +1,18 @@
+import { Container, Form } from "semantic-ui-react";
+import { AffirmActionButton } from "../ui";
+
 export const ResetPassword = () => {
-  return <div className="reset-password-container"></div>;
+  return (
+    <Container>
+      <h2>Reset Password</h2>
+      <div className="reset-password-container"></div>
+      <Form>
+        <Form.Field>
+          <label>Email</label>
+          <input placeholder="Email" />
+        </Form.Field>
+        <AffirmActionButton>Submit</AffirmActionButton>
+      </Form>
+    </Container>
+  );
 };
