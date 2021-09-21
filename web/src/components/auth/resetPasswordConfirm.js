5b9dc84b686b0d722fcc8f58e3d20d3bd204d292
Alana Tran <alanatran@Hues-MacBook-Pro.local>
2021-09-20 20:59:13 -0700

    beginning work on password pages

--- web/src/components/auth/resetPasswordConfirm.js
+++ web/src/components/auth/resetPasswordConfirm.js
@@ -1,3 +1,13 @@
+import { Container } from "semantic-ui-react";
+import { SuccessMessage } from "../ui";
+
 export const ResetPasswordConfirm = () => {
-  return <div className="reset-password-confirm-container"></div>;
+  return (
+    <Container>
+      <SuccessMessage>Success! Your password has been reset.</SuccessMessage>
+        <div className="reset-password-confirm-container">
+          <h4>You can now log in with your new password.</h4>
+        </div>;
+    </Container>
+  );
 };
