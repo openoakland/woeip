bf6cc9035091e199cb5d47388c443164c6556b1a
Timothy Miller <miller.tim108@gmail.com>
2022-02-28 13:01:04 -0500

    create logout component

---
+++ web/src/components/auth/logout.js
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+// TODO: logout and return to Login page
+export const Logout = (setAuthToken) => {
+    useEffect(() => {
+        return 
+    }, [])
+    return <></>
+};
