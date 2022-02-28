17051afadc45ae57a91204839f8df4cf69f4b5c6
Timothy Miller <miller.tim108@gmail.com>
2022-02-27 20:48:41 -0500

    remove templates

--- api/woeip/settings.py
+++ api/woeip/settings.py
@@ -69,23 +69,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "woeip.urls"
 
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [str(project_root.path("templates"))],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.template.context_processors.i18n",
-                "django.contrib.messages.context_processors.messages",
-            ]
-        },
-    }
-]
-
 WSGI_APPLICATION = "woeip.wsgi.application"
 
 # Database
