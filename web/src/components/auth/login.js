e0c5add59c84e74adee8b000474b63c7b5d57ac3
Timothy Miller <miller.tim108@gmail.com>
2022-02-28 22:19:56 -0500

    loader for log in and out

--- web/src/components/auth/login.js
+++ web/src/components/auth/login.js
@@ -2,15 +2,17 @@ import { useContext, useState } from "react";
 import { AuthTokenContext } from "./tokenContext";
 import { login, setAuthTokenItem } from "./utils";
 import { Link, useHistory } from "react-router-dom";
-import { Container } from "../ui";
+import { Container, Dimmer, Loader } from "../ui";
 
 export const Login = () => {
   const { setAuthToken } = useContext(AuthTokenContext);
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const history = useHistory();
 
   const handleSubmit = async (e) => {
+    setIsLoading(true);
     e.preventDefault();
     const { errored, code, token } = await login(email, password);
     if (!errored && code === 200) {
@@ -18,6 +20,7 @@ export const Login = () => {
       setAuthTokenItem(token);
       history.push("/");
     }
+    setIsLoading(false);
   };
 
   const changeEmail = (e) => setEmail(e.target.value);
@@ -25,6 +28,10 @@ export const Login = () => {
 
   return (
     <Container textAlign="center">
+      <Dimmer active={isLoading}>
+        <Loader indeterminate />
+        Logging In...
+      </Dimmer>
       <h2>Login to continue</h2>
       <Link to={"/auth/register"}>Create an Account</Link>
       <form onSubmit={handleSubmit}>
--- web/src/components/auth/logout.js
+++ web/src/components/auth/logout.js
@@ -2,18 +2,16 @@ import { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { AuthTokenContext } from "./tokenContext";
 import { clearAuthTokenItem, logout } from "./utils";
-import { Container } from "../ui";
+import { Container, Dimmer, Loader } from "../ui";
 
 export const Logout = () => {
   const { authToken, setAuthToken } = useContext(AuthTokenContext);
   const [pending, setPending] = useState(true);
-  const [errored, setErrored] = useState(false);
   const history = useHistory();
 
   useEffect(() => {
     (async () => {
       const { errored, code } = await logout(authToken);
-      setErrored(errored);
       setPending(false);
       clearAuthTokenItem();
       setAuthToken("");
@@ -25,8 +23,9 @@ export const Logout = () => {
   return (
     // TODO: Loading and Error feedback
     <Container textAlign="center">
-      {pending ? "pending\n" : "not pending"}
-      {errored ? "errored" : "not errored "}
+      <Dimmer active={pending}>
+        <Loader indeterminate>Logging out...</Loader>
+      </Dimmer>
     </Container>
   );
 };
