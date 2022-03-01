0ebc13e2d943c63c4a4bd32df4c02653c7e5295f
Timothy Miller <miller.tim108@gmail.com>
2022-02-28 20:10:03 -0500

    basic password match feedback

--- web/src/components/auth/register.js
+++ web/src/components/auth/register.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { AuthTokenContext } from "./tokenContext";
 import { register, setAuthTokenItem } from "./utils";
 import { Link, useHistory } from "react-router-dom";
@@ -10,10 +10,15 @@ export const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordsMismatched, setPasswordsMismatched] = useState(false);
 
   const changeEmail = (e) => setEmail(e.target.value);
   const changePassword = (e) => setPassword(e.target.value);
   const changeConfirmPassword = (e) => setConfirmPassword(e.target.value);
+
+  useEffect(() => {
+    setPasswordsMismatched(confirmPassword && password !== confirmPassword);
+  }, [password, confirmPassword]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -51,6 +56,7 @@ export const Register = () => {
             value={confirmPassword}
             onChange={changeConfirmPassword}
           />
+          <p>{passwordsMismatched && "passwords do not match"}</p>
         </label>
         <input type="submit" value="Register" />
       </form>
