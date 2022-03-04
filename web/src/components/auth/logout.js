e2bae1d7d5bf7d371559202f995b258524e86760
Timothy Miller <miller.tim108@gmail.com>
2022-03-03 19:14:22 -0500

    Revert "remove TODO for logout feedback"
    This reverts commit 09f790dea86ab057b93db0935306063f4eb6036e.

--- web/src/components/auth/logout.js
+++ web/src/components/auth/logout.js
@@ -21,6 +21,7 @@ export const Logout = () => {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
+    // TODO: Loading and Error feedback
     <Container textAlign="center">
       <Dimmer active={pending}>
         <Loader indeterminate>Logging out...</Loader>
