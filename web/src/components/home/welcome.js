0b187a43c59eb2229e3100f68855cda6a166c3a1
Timothy Miller <miller.tim108@gmail.com>
2022-03-19 11:38:14 -0400

    welcome width

--- web/src/components/home/welcome.js
+++ web/src/components/home/welcome.js
@@ -1,13 +1,14 @@
 import { Container } from "../ui";
+import './welcome.css';
 
 export default function Welcome() {
   return (
     <Container>
-      <h1>Welcome to West Oakland Air Quality!</h1>
-      <p>
+      <h1>Welcome to WOAQ!</h1>
+      <p className="welcome-intro">
         West Oakland Air Quality supports the West Oakland Environmental
         Indicators Project by putting local air quality in the hands of West
-        Oaklanders
+        Oaklanders.
       </p>
     </Container>
   );
