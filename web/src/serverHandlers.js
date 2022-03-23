a24e1a8e96b359c5928b4bfba8dd85e0617192c7
Gabriel Ehrlich <gabriel.s.ehrlich@gmail.com>
2022-03-22 21:02:51 -0700

    Lint serverHandlers.js

--- web/src/serverHandlers.js
+++ web/src/serverHandlers.js
@@ -10,35 +10,35 @@ export const testData = {
       starts_at: "2022-01-01T12:34:56Z",
       ends_at: "2022-01-01T15:43:21Z",
       collection_files: [
-        'http://api.fake.site/collection_files/1',
-        'http://api.fake.site/collection_files/2'
-      ]
+        "http://api.fake.site/collection_files/1",
+        "http://api.fake.site/collection_files/2",
+      ],
     },
     {
       id: 2,
       starts_at: "2022-01-02T12:34:56Z",
       ends_at: "2022-01-02T15:43:21Z",
       collection_files: [
-        'http://api.fake.site/collection_files/3',
-        'http://api.fake.site/collection_files/4'
-      ]
+        "http://api.fake.site/collection_files/3",
+        "http://api.fake.site/collection_files/4",
+      ],
     },
     {
       id: 3,
       starts_at: "2022-01-19T12:34:56Z",
       ends_at: "2022-01-19T15:43:21Z",
       collection_files: [
-        'http://api.fake.site/collection_files/5',
-        'http://api.fake.site/collection_files/6'
-      ]
-    }
+        "http://api.fake.site/collection_files/5",
+        "http://api.fake.site/collection_files/6",
+      ],
+    },
   ],
-  collectionDates: [ '2022-01-01', '2022-01-02', '2022-01-19' ]
+  collectionDates: ["2022-01-01", "2022-01-02", "2022-01-19"],
 };
 
 const handlers = [
   rest.get(apiUrlCollections(), async (_req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(testData['collections']));
+    return res(ctx.status(200), ctx.json(testData["collections"]));
   }),
 
   rest.get(apiUrlCollectionById("*"), (_req, res, ctx) => {
