d8439f1e1d7fb798a15bd43fed9fffc63be0388c
Gabriel Ehrlich <gabriel.s.ehrlich@gmail.com>
2022-03-22 20:47:06 -0700

    Add placeholder test for automatically setting date to most recent date with data

--- web/src/components/map/menu.test.js
+++ web/src/components/map/menu.test.js
@@ -49,6 +49,9 @@ describe("MapMenu", () => {
     );
   });
 
+  it("should automatically set the date to the most recent data-having date", () =>
+    "Test not yet implemented. How to? This behavior is caused by an effect hook in index.js");
+
   it("should render a single collection", () => {
     const collectionsOnDate = [firstCollection];
     const activeId = firstCollection.id;
