23c2bf89b6b313fe8590d4bcf3a753cf40394b34
Timothy Miller <miller.tim108@gmail.com>
2022-03-27 15:20:38 -0400

    token local storage tests

--- web/src/components/auth/utils.test.js
+++ web/src/components/auth/utils.test.js
@@ -1,3 +1,5 @@
+import { clearAuthTokenItem, getAuthTokenItem, setAuthTokenItem } from "./utils";
+
 describe("login", () => {
     it.todo("should return a token on success");
     it.todo("should return an empty string on failure");
@@ -13,14 +15,55 @@ describe("register", () => {
     it.todo("should return empty string on failure");
 })
 
-describe("clearAuthToken", () => {
-    it.todo("should remove auth token from local storage");
-});
 
-describe("getAuthTokenItem", () => {
-    it.todo("should retrieve an auth token from local storage");
-});
+describe("token local storage manipulation",() => {
+    beforeEach(() => {
+        const fakeLocalStorage = (function () {
+          let store = {};
+    
+          return {
+            getItem: function (key) {
+              return store[key] || null;
+            },
+            setItem: function (key, value) {
+              store[key] = value.toString();
+            },
+            removeItem: function (key) {
+              delete store[key];
+            },
+            clear: function () {
+              store = {};
+            },
+          };
+        })();
+    
+        Object.defineProperty(window, "localStorage", {
+          value: fakeLocalStorage,
+        });
+      });
+    
+      afterEach(() => {
+        localStorage.clear();
+        jest.restoreAllMocks();
+      });
 
-describe("setAuthTokenItem", () => {
-    it.todo("should set the auth token in local storage");
-});
+    it("should remove auth token from local storage", () => {
+        const removeItemSpy = jest.spyOn(localStorage, "removeItem");
+        clearAuthTokenItem();
+        expect(removeItemSpy).toHaveBeenCalledWith("authToken");
+    });
+
+    it("should retrieve an auth token from local storage", () => {
+        const getItemSpy = jest.spyOn(localStorage, "getItem");
+        const authTokenItem = getAuthTokenItem();
+        expect(authTokenItem).toBe('');
+        expect(getItemSpy).toHaveBeenCalledWith("authToken");
+    });
+
+    it("should set the auth token in local storage", () => {
+        const setItemSpy = jest.spyOn(localStorage, "setItem");
+        setAuthTokenItem("fakeToken");
+        expect(setItemSpy).toHaveBeenCalledWith("authToken", "fakeToken");
+    });
+})
+
