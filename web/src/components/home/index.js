e18f4eee996c7d6819c584838acecaa294fb5329
Jess Sand <32755410+theecrit@users.noreply.github.com>
2022-05-13 14:48:06 -0700

    Update component formatting

--- web/src/components/home/index.js
+++ web/src/components/home/index.js
@@ -1,6 +1,7 @@
 import { Container } from "../ui";
 import "./index.css";
 
+
 export const Home = () => {
   return (
 
@@ -12,17 +13,34 @@ export const Home = () => {
           West Oakland Air Quality Project
         </h1>
         <h2>Explore local air quality to understand the impact on you, your family, and your neighborhood.</h2>
-        <div class="spacer-button"><a href="#" class="ui button test">Explore the map <i class="right arrow icon"></i></a></div>
+        <div class="spacer-button"><a href="/maps" class="ui button test">Explore the map <i class="right arrow icon"></i></a></div>
       </div>
     </div>
 
-  <div class="ui container about-container">
-    <h2 className="ui header">Start collecting data yourself</h2>
-    <p>
-      Want to join our team of volunteer data collectors?
-      <div class="spacer-button"><a href="#" class="ui button test">Sign up for WOEIP's next training</a></div>
-    </p>
-  </div>
+    <div class="ui center aligned equal width grid">
+      <div class="column">
+        <div class="ui"></div>
+      </div>
+      <div class="eight wide column">
+        <div class="ui">
+          <div class="ui text-container about-container">
+            <h2 className="ui header">Start collecting data yourself</h2>
+            <p>
+              Want to join our team of volunteer data collectors?
+              <div class="spacer-button"><a href="/maps" class="ui button violet">Sign up for WOEIP's next training</a></div>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="ui"></div>
+      </div>
+    </div>
+
+
+
+
+
 
 
     </Container>
