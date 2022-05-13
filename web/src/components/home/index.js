a4deb3c011f2008874509f7a385f3be99a6c7bc8
Jess Sand <32755410+theecrit@users.noreply.github.com>
2022-05-13 14:27:52 -0700

    Add content to home page; tweak site-wide font to use Semantic defaults

--- web/src/components/home/index.js
+++ web/src/components/home/index.js
@@ -1,9 +1,30 @@
 import { Container } from "../ui";
+import "./index.css";
 
 export const Home = () => {
   return (
-    <Container textAlign="center">
-      <h2>Welcome to West Oakland Air Quality!</h2>
+
+    <Container>
+
+    <div class="ui container inverted vertical masthead center aligned segment teal">
+      <div class="ui text container spacer">
+        <h1 class="ui inverted header">
+          West Oakland Air Quality Project
+        </h1>
+        <h2>Explore local air quality to understand the impact on you, your family, and your neighborhood.</h2>
+        <div class="spacer-button"><a href="#" class="ui button test">Explore the map <i class="right arrow icon"></i></a></div>
+      </div>
+    </div>
+
+  <div class="ui container about-container">
+    <h2 className="ui header">Start collecting data yourself</h2>
+    <p>
+      Want to join our team of volunteer data collectors?
+      <div class="spacer-button"><a href="#" class="ui button test">Sign up for WOEIP's next training</a></div>
+    </p>
+  </div>
+
+
     </Container>
   );
 };
