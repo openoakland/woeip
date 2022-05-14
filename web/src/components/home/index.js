217d36d94a9e6451c394ac6ec5cb3a51457fe58d
Jess Sand <32755410+theecrit@users.noreply.github.com>
2022-05-14 12:38:12 -0700

    type styling & initial woeip logo placement

--- web/src/components/home/index.js
+++ web/src/components/home/index.js
@@ -6,7 +6,7 @@ export const Home = () => {
 
     <Container>
 
-    <div class="ui container inverted vertical masthead center aligned segment hero">
+    <div class="ui container inverted vertical masthead left aligned segment hero">
       <div class="ui text container spacer">
         <h1 class="ui inverted header">
           West Oakland Air Quality Project
@@ -16,24 +16,25 @@ export const Home = () => {
       </div>
     </div>
 
-    <div class="ui center aligned equal width grid">
+    <div class="ui left aligned equal width grid">
+
       <div class="column">
         <div class="ui"></div>
       </div>
+
       <div class="eight wide column">
         <div class="ui">
           <div class="ui text-container about-container">
+          <div class="ui small image"><img src="./WOEIP-mark-2020-transp-225x220px.png"/></div>
+            <div>
             <h2 className="ui header">Start collecting data yourself</h2>
-            <p>
-              Want to join WOEIP's team of volunteer data collectors?
-
-              <div class="spacer-button"><a href="http://eepurl.com/hqw6RD" target="_blank" class="ui button">Sign up for volunteer opportunities &nbsp;<i class="external square alternate icon"></i></a></div>
-
-
-            </p>
+            <p>Want to join WOEIP's team of volunteer data collectors?</p>
+            <p><div><a href="http://eepurl.com/hqw6RD" target="_blank" class="ui button">Sign up for volunteer opportunities &nbsp;<i class="external square alternate icon"></i></a></div></p>
+            </div>
           </div>
         </div>
       </div>
+
       <div class="column">
         <div class="ui"></div>
       </div>
