4c7421bf2cc422ab6e0faf79ae9bfe1d6656b2ee
Jess Sand <32755410+theecrit@users.noreply.github.com>
2022-05-14 13:48:49 -0700

    Troubleshoot image rendering; adjust responsive formatting

--- web/src/components/home/index.js
+++ web/src/components/home/index.js
@@ -1,5 +1,6 @@
-import { Container } from "../ui";
+import { Container, Image } from "../ui";
 import "./index.css";
+import woeipLogo from './WOEIP-mark-2020-transp-225x220px.png';
 
 export const Home = () => {
   return (
@@ -8,36 +9,22 @@ export const Home = () => {
 
     <div class="ui container inverted vertical masthead left aligned segment hero">
       <div class="ui text container spacer">
-        <h1 class="ui inverted header">
-          West Oakland Air Quality Project
-        </h1>
+        <h1 class="ui inverted header">West Oakland Air Quality Project</h1>
         <h2>Explore local air quality to understand the impact on you, your family, and your neighborhood.</h2>
         <div class="spacer-button"><a href="/maps" class="ui button teal">Explore the map <i class="right arrow icon"></i></a></div>
       </div>
     </div>
 
-    <div class="ui left aligned equal width grid">
+    <div class="ui grid">
 
-      <div class="column">
-        <div class="ui"></div>
-      </div>
-
-      <div class="eight wide column">
-        <div class="ui">
+      <div class="eight wide computer twelve wide tablet sixteen wide mobile centered column">
           <div class="ui text-container about-container">
-          <div class="ui small image"><img src="./WOEIP-mark-2020-transp-225x220px.png"/></div>
-            <div>
-            <h2 className="ui header">Start collecting data yourself</h2>
-            <p>Want to join WOEIP's team of volunteer data collectors?</p>
-            <p><div><a href="http://eepurl.com/hqw6RD" target="_blank" class="ui button">Sign up for volunteer opportunities &nbsp;<i class="external square alternate icon"></i></a></div></p>
-            </div>
+              <h2 className="ui header">Start collecting data yourself</h2>
+              <p>Want to join WOEIP's team of volunteer data collectors?</p>
+              <p><img class="ui mini image left floated" src={ woeipLogo } alt="WOEIP weathervane logo"/><a href="http://eepurl.com/hqw6RD" target="_blank" class="ui button">Learn about volunteer opportunities &nbsp;<i class="external square alternate icon"></i></a></p>
           </div>
-        </div>
       </div>
 
-      <div class="column">
-        <div class="ui"></div>
-      </div>
     </div>
 
 
