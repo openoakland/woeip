101803256aa08d6dc6fe1357996000a87fc93671
Gabriel Ehrlich <gabriel.s.ehrlich@gmail.com>
2022-11-17 16:20:03 -0800

    Fixed failing tests

--- web/src/components/footer/index.js
+++ web/src/components/footer/index.js
@@ -16,33 +16,36 @@ export const Footer = () => {
       <Grid.Column>
         <Menu secondary>
           <Menu.Item
+            data-testid="footer-link-github"
             href="https://github.com/openoakland/woeip"
             position="left"
             {...sharedMenuItemProps}
           >
-            <Image src={woaqLogo} spaced="right"/> on <Image src={gitHubLogo} spaced="left" id="githubFooterLink"/>
+            <Image src={woaqLogo} spaced="right" alt="WOAQ logo"/> on <Image src={gitHubLogo} spaced="left" id="footerImgGithub" alt="GitHub logo"/>
           </Menu.Item>
         </Menu>
       </Grid.Column>
       <Grid.Column>
         <Menu secondary>
           <Menu.Item
+            data-testid="footer-link-oo"
             className="center" // could add this to UI
             href="https://openoakland.org"
             {...sharedMenuItemProps}
           >
-            Created by <Image src={ooLogo}/>
+            Created by <Image src={ooLogo} alt="Open Oakland logo"/>
           </Menu.Item>
         </Menu>
       </Grid.Column>
       <Grid.Column>
         <Menu fluid secondary>
           <Menu.Item
+            data-testid="footer-link-woeip"
             href="https://woeip.org"
             position="right"
             {...sharedMenuItemProps}
           >
-            <Image src={woeipLogo}/>
+            <Image src={woeipLogo} alt="WOEIP name and weathervane logo"/>
           </Menu.Item>
         </Menu>
       </Grid.Column>
